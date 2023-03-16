ae9a9c657de89e3396a7104c32191f5b12896b6e
JulianBristol <82460914+JulianBristol@users.noreply.github.com>
2023-03-16 14:41:21 -0400

    Update header with styling

--- client/src/header/Header.jsx
+++ client/src/header/Header.jsx
@@ -1,27 +1,39 @@
 import React from 'react';
+import Icon from '../components/Icon Files/Icon.jsx';
+import IconText from '../components/Icon Files/IconText.jsx';
+import './Header.css';
 
 const Header = () => (
     <div id='headerBar' style={{ height: '92px' }}>
-        <img
-        src=''
-        alt=''
-        />
-      <nav>
+
+        <div className='navDiv'>
+            <div className='icon'>
+            <a href='/'>
+        <Icon />
+        <IconText/>
+        </a>
+        </div>
+        <nav>
         <ul>
-            <li>
-                about
-            </li>
-            <li>
-                portfolio
-            </li>
-            <li>
-                blog
-            </li>
-            <li>
-                contact
-            </li>
+            <a href='about'><li>
+                <span className='firstLetter'>a</span>BOUT
+                <div className='textReflection'><span className='firstLetter'>a</span>BOUT</div>
+            </li></a>
+            <a href='portfolio'><li>
+            <span className='firstLetter'>p</span>ORTFOLIO
+                <div className='textReflection'><span className='firstLetter'>p</span>ORTFOLIO</div>
+            </li></a>
+            <a href='blog'><li>
+            <span className='firstLetter'>b</span>LOG
+                <div className='textReflection'><span className='firstLetter'>b</span>LOG</div>
+            </li></a>
+            <a href='contact'><li>
+            <span className='firstLetter'>c</span>ONTACT
+                <div className='textReflection'><span className='firstLetter'>c</span>ONTACT</div>
+            </li></a>
         </ul>
       </nav>
+      </div>
     </div>
 );
 
