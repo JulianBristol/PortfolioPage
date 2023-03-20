66a497ddea47d24b4d2733452fbd679874083cca
JulianBristol <82460914+JulianBristol@users.noreply.github.com>
2023-03-20 15:26:41 -0400

    Add content and styling to Home section

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,32 +1,48 @@
-import React from 'react';
+/* eslint-disable max-len */
+import React, { useEffect } from 'react';
+import Julian from '../images/Good Enough.png';
+import './Home.css';
 
-const Home = () => (
-        <div id="home" style={{ width: '100%' }}>
-            <div className="rightDiv">
-            <h1>Hey, it&apos;s me,
-                <div id="name"><span className='emph1'> Julian</span> <span className="emph2">Bristol</span></div>
+const Home = () => {
+  useEffect(() => {
+    document.getElementById('welcome').style.opacity = 1;
+  }, []);
+
+  return (
+        <div id="welcome">
+            <div className='backgroundBlur'>
+<div className='welcomeMessage'>
+
+    <div className='wcRight'>
+        <div className='wcImgBG A' style={{ backgroundColor: '#360A11' }}>
+            </div>
+        <div className='wcImgBG B'>
+        </div>
+        <div className='wcImgBG C'>
+            <img className='wcImg'
+            src={Julian}
+            alt="The sexiest being on the planet"
+            />
+            </div>
+    </div>
+    <div className='wcLeft'>
+        <div className='text'>
+    <h1>Hey, it&apos;s me,
+                <div className='JulianBristol'><span className='emph1'>J</span>ulian <span className="emph1">B</span>ristol</div>
                 </h1>
-                <p>I&apos;m a <span className='jobTitle'>Full-Stack Developer</span> who is passionate about creating
+                <p>I&apos;m a <span className='jobTitle'>Full-Stack
+                Developer</span> who is passionate about creating
                 interactive web applications as well as developing interesting software and experiences.</p>
-                <div id='btnLinks'>
+                </div>
+                <div className='btnLinks'>
                 <button>Resume()</button>
                 <button>LinkedIn</button>
                 <button>GitHub</button>
                 </div>
-                <div className="leftDiv">
-                    <img
-                    src="https://avatars.githubusercontent.com/u/82460914?v=4"
-                    alt="The sexiest being on the planet"
-                    style={{ borderRadius: '50%',
-                      marginLeft: '300px',
-                      width: '350px',
-                      height: '350px',
-                      boxShadow: '-10px 0 #8c162755, -20px 0 #a4674255, -30px 0 #bfbe5f55',
-                    }}
-                    />
-                </div>
-            </div>
+    </div>
+</div>
+</div>
         </div>
-);
+  ); };
 
 export default Home;
