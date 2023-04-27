2ab461ce95fd861791a3ac91402e26eef6b88d48
JulianBristol <82460914+JulianBristol@users.noreply.github.com>
2023-04-27 13:42:21 -0400

    Make changes to the node installations

--- client/src/JS_MASTERY_Components/index.js
+++ client/src/JS_MASTERY_Components/index.js
@@ -1,7 +1,5 @@
 /* eslint-disable */
 import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
-import Hero from './Hero';
-import Navbar from './Navbar';
 import About from './About';
 import Tech from './Tech';
 import Experience from './Experience';
@@ -10,8 +8,6 @@ import Feedbacks from './Feedbacks';
 import Contact from './Contact';
 
 export {
-  Hero,
-  Navbar,
   About,
   Tech,
   Experience,
