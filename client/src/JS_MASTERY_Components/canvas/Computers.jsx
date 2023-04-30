e069a7cea0ec73fa98914e4dc42c11e803d3f381
JulianBristol <82460914+JulianBristol@users.noreply.github.com>
2023-04-30 15:24:08 -0400

    Eat Avocado Toast

--- client/src/JS_MASTERY_Components/canvas/Computers.jsx
+++ client/src/JS_MASTERY_Components/canvas/Computers.jsx
@@ -1,10 +1,87 @@
 /* eslint-disable */
-import React from 'react'
+import { Suspense, useEffect, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import CanvasLoader from '../Loader';
+import { Object3D } from 'three';
 
 const Computers = () => {
+  const computer = useGLTF('./desktop_pc/scene.gltf');
+
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={0.15}
+      groundColor='red' />
+      <pointLight intensity={1} />
+      <primitive
+      object={computer.scene}
+      scale={3.75}
+      />
+    </mesh>
   )
 }
 
-export default Computers
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameloop='demand'
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25}}
+      gl={{ preserveDrawingBuffer: true }}
+      style={{ outline: '1px dashed red', height: '300px'}}
+    >
+      <Suspense /* fallback={<CanvasLoader />} */>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  )
+}
+
+
+const AvocadoToast = () => {
+  const toast = useGLTF('./avocadoToast/scene.gltf');
+  //Create a new pivot point
+  const pivot = new Object3D();
+  pivot.position.set(0, 0, -6);
+  pivot.add(toast.scene)
+
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.15}
+      groundColor='black' />
+      <pointLight intensity={1} />
+      <group position={[0, 0.25, -1.25]}>
+      <primitive object={pivot} scale={6.75} />
+    </group>
+    </mesh>
+  )
+}
+const AvocadoToastCanvas = () => {
+  return (
+    <Canvas
+      frameloop='demand'
+      shadows
+      camera={{ position: [20, 3, 4], fov: 25, }}
+      gl={{ preserveDrawingBuffer: true }}
+      style={{ outline: '1px dashed red', height: '300px'}}
+    >
+      <Suspense /* fallback={<CanvasLoader />} */>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 3.5}
+          minPolarAngle={Math.PI / 3.5}
+        />
+        <AvocadoToast />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  )
+}
+
+export default AvocadoToastCanvas
--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@
 import React, { useEffect } from 'react';
 import Julian from '../../images/Good Enough.png';
 import './Hero.css';
+import { ComputersCanvas } from '../../JS_MASTERY_Components';
 
 const Hero = () => {
   useEffect(() => {
@@ -9,42 +10,42 @@ const Hero = () => {
   }, []);
 
   return (
+    <div>
         <div id="welcome">
             <div className='backgroundBlur'>
-<div className='welcomeMessage'>
-    <div className='welcomeContent'>
+                <div className='welcomeMessage'>
+                    <div className='welcomeContent'>
 
-        <div className='welcomeLeft'>
-            <div className='welcomeImgBG A'>
+                        <div className='welcomeLeft'>
+                            <div className='welcomeImgBG A'></div>
+                            <div className='welcomeImgBG B'></div>
+                            <div className='welcomeImgBG C'>
+                                <img className='welcomeImg'
+                                src={Julian}
+                                alt="The sexiest being on the planet"/>
+                            </div>
+                        </div>
+
+                        <div className='welcomeRight'>
+                            <div className='text'>
+                                <h1>Hey, it&apos;s me,
+                                    <div className='JulianBristol'><span className='emph1'>J</span>ulian <span className="emph1">B</span>ristol</div>
+                                </h1>
+                                <p>I&apos;m a <span className='jobTitle'>Full-Stack Developer</span> who is passionate about creating interactive web applications as well as developing interesting software and experiences.</p>
+                            </div>
+                            <div className='btnLinks'>
+                                <button>Resume()</button>
+                                <button>LinkedIn</button>
+                                <button>GitHub</button>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
-            <div className='welcomeImgBG B'>
             </div>
-            <div className='welcomeImgBG C'>
-                <img className='welcomeImg'
-                src={Julian}
-                alt="The sexiest being on the planet"
-                />
-                </div>
         </div>
-        <div className='welcomeRight'>
-            <div className='text'>
-        <h1>Hey, it&apos;s me,
-                    <div className='JulianBristol'><span className='emph1'>J</span>ulian <span className="emph1">B</span>ristol</div>
-                    </h1>
-                    <p>I&apos;m a <span className='jobTitle'>Full-Stack
-                    Developer</span> who is passionate about creating
-                    interactive web applications as well as developing interesting software and experiences.</p>
-                    </div>
-                    <div className='btnLinks'>
-                    <button>Resume()</button>
-                    <button>LinkedIn</button>
-                    <button>GitHub</button>
-                    </div>
-                    </div>
-        </div>
-</div>
-</div>
-        </div>
+        <ComputersCanvas />
+    </div>
   ); };
 
 export default Hero;
