c69b451fd19dadc181df4510cce801866ef7da2a
JulianBristol <82460914+JulianBristol@users.noreply.github.com>
2023-05-01 16:39:19 -0400

    Add responsiveness to Hero section

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -40,7 +40,7 @@ const Hero = () => {
                                 <p>Where does the &quot;evil scientist&quot; part fit in?  Well, I was the type of kid who enjoyed taking apart electronic devices and game systems just to see what was inside (and maybe to steal their magnets and lasers). Over time, that destructive search for knowledge mellowed out into a more systematic approach.
                                 </p>
                                 <p>What does this mean for you? I crave to learn new things, whether they be software, processes, or systems. Teach me how to do something and I will do it better and faster than you expected. I am a hard worker who goes above and beyond not because it is expected of me, but rather because that is who I am.</p>
-</div>
+                                </div>
                                 </div>
                                 </div>
                                 <button className={`toggleTextBtn ${toggleText ? 'textLeft' : 'textRight'}`} onClick={() => {
@@ -48,14 +48,14 @@ const Hero = () => {
                                 }
                                 }>{toggleText ? 'Next' : 'Back'}</button>
                             </div>
+                        </div>
+
+                    </div>
                             <div className={`btnLinks ${toggleText ? 'btnPos1' : 'btnPos2'}`}>
                                 <button>Resume()</button>
                                 <button>LinkedIn</button>
                                 <button>GitHub</button>
                             </div>
-                        </div>
-
-                    </div>
                 </div>
             </div>
         </div>
