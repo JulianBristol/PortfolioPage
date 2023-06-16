72bf2b33716da999f5b79d772cc5684149cb5be5
JulianBristol <82460914+JulianBristol@users.noreply.github.com>
2023-06-16 11:35:39 -0400

    Make Avaocado Toast file smaller

--- client/src/components/Current/Current.jsx
+++ client/src/components/Current/Current.jsx
@@ -12,10 +12,15 @@ const Current = () => (
         <h2 className='sectionTitle'>What Am I Learning Now?</h2>
         </motion.div>
             <motion.div className='currentStudies' variants={fadeIn('', '', 0.1, 1)}>
-                <p>I&apos;m on a mission to master <span>threeJS</span> and <span>machine learning</span>.</p>
+                {/* eslint-disable-next-line max-len */}
+                <p>I&apos;m on a mission to learn about <span><a href='https://microsoft.github.io/AI-For-Beginners/' target='_blank' rel='noreferrer'>AI Development</a></span> and attain a certification in <span><a href='https://academy.hubspot.com/courses/seo-training' target='_blank' rel='noreferrer'>SEO</a></span>.</p>
+                {/* <p>I&apos;m on a mission to master <span>threeJS</span> and <span>machine learning</span>.</p> */}
                 <br/>
                 {/* eslint-disable-next-line max-len */}
                 <p>If you&apos;re in need of a talented and enthusiastic developer who&apos;s fueled by a slightly unhealthy amount of avocado toast, then look no further!</p>
+                {/* <p>But wait there&apos;s more:</p> */}
+                {/* eslint-disable-next-line max-len */}
+                {/* <p>While I am skilled in web development, my skills in React Native need some polish. My next large project will be something interesting on mobile.</p> */}
             </motion.div>
     </div>
     <div className='containerManager'>
