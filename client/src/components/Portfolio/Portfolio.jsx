d27eba9b2df97ae5401b1e07ff67a5b974532bb1
JulianBristol <82460914+JulianBristol@users.noreply.github.com>
2023-10-24 14:21:18 -0400

    Minor Bug Fixes

--- client/src/components/Portfolio/Portfolio.jsx
+++ client/src/components/Portfolio/Portfolio.jsx
@@ -8,8 +8,29 @@ import { github } from '../../assets';
 import { projects } from '../../constants';
 import './PortfolioCSS.css';
 
-const Portfolio = () => {
-  const ProjectCard = ({ index, name, description, tags, image, sourceCodeLink, activeLink }) => (
+const Portfolio = () => (
+    <div className='glassmorphism' style={{ maxWidth: '1500px' }}>
+      {/* Original */}
+        <motion.div variants={textVariant()} style={{ display: 'grid' }}>
+            <h2 className='sectionTitle'>Check Out My Projects</h2>
+        </motion.div>
+        <motion.p className='sectionDesc' variants={fadeIn('', '', 0.1, 1)}>
+            {/* eslint-disable-next-line max-len */}
+            These projects demonstrate my skills and experience with real-life examples of my work. Each project is described briefly and includes links to where you can see the code and how it works. They show how I can solve tough problems, work with different technologies, and lead projects well.
+        </motion.p>
+        <div className='projectCardGroup'>
+            {projects.map((project, index) => (
+                <ProjectCard
+                    key={`project-${index}`}
+                    index={index}
+                    {...project}
+                />
+            ))}
+        </div>
+    </div>
+);
+
+const ProjectCard = ({ index, name, description, tags, image, sourceCodeLink, activeLink }) => (
         <motion.div variants={fadeIn('up', 'spring', index * 0.5, 0.75)}>
             <Tilt
                 options={{
@@ -26,7 +47,7 @@ const Portfolio = () => {
                     window.open(activeLink, '_blank');
                   }
                 }}>
-                <div className='cardImgContainer'>
+                  <div className='cardImgContainer'>
                     <img
                     className='cardImg'
                     src={image}
@@ -51,7 +72,7 @@ const Portfolio = () => {
                 <div className='skillTags'>
                     {tags.map((tag) => (
                         <p key={tag.name} style={{ color: tag.color }}>
-                            #{tag.name}
+                            {tag.name}
                         </p>
                     ))}
                 </div>
@@ -59,36 +80,14 @@ const Portfolio = () => {
                 </div>
             </Tilt>
         </motion.div>
-  );
+);
 
-  return (
-    <div className='glassmorphism' style={{ maxWidth: '1500px' }}>
-        <motion.div variants={textVariant()} style={{ display: 'grid' }}>
-            <h2 className='sectionTitle'>Check Out My Projects</h2>
-        </motion.div>
-        <motion.p className='sectionDesc' variants={fadeIn('', '', 0.1, 1)}>
-            {/* eslint-disable-next-line max-len */}
-            These projects demonstrate my skills and experience with real-life examples of my work. Each project is described briefly and includes links to where you can see the code and how it works. They show how I can solve tough problems, work with different technologies, and lead projects well.
-        </motion.p>
-        <div className='projectCardGroup'>
-            {projects.map((project, index) => (
-                <ProjectCard
-                    key={`project-${index}`}
-                    index={index}
-                    {...project}
-                />
-            ))}
-        </div>
-    </div>
-  );
-};
-
-Portfolio.propTypes = {
+ProjectCard.propTypes = {
   index: PropTypes.number,
   name: PropTypes.string,
   description: PropTypes.string,
-  tags: PropTypes.arrayOf(PropTypes.string),
-  image: PropTypes.object,
+  tags: PropTypes.arrayOf(PropTypes.object),
+  image: PropTypes.string,
   sourceCodeLink: PropTypes.string,
   activeLink: PropTypes.string,
 };
